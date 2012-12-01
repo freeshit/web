dcaeacc603818e668bb0691a695d1ea4487eaa06
Blake Taylor <blakefrost@gmail.com>
2012-12-01 13:32:47 -0800

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freeshit::Application.config.secret_token = '998bed565d9fc392f3f8eb98c2049a497aece3075927ce18511154da6e5ab70f71a81d5b10385c01523e971673940dd9e9ee6cbe35f80c72691836200192e0f3'
